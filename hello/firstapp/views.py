72fd23094461679e4b5650296632499143e18957
asya <asya.gailamazyan@yandex.ru>
2022-02-26 11:35:53 +0300

    in hello world added status handling

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import (
+    HttpResponse,
+    HttpResponseRedirect,
+    HttpResponsePermanentRedirect,
+    HttpResponseNotModified,
+    HttpResponseBadRequest,
+    HttpResponseForbidden,
+    HttpResponseNotFound,
+    HttpResponseNotAllowed,
+    HttpResponseServerError,
+    HttpResponseGone,
+)
 
 
 def hello_world(request):
@@ -33,3 +44,30 @@ def users(request):
     output = "<h2>User</h2><h3>id: {0}  name: {1}</h3>".format(id, name)
     return HttpResponse(output)
 
+
+def m304(request):
+    return HttpResponseNotModified()
+
+
+def m400(request):
+    return HttpResponseBadRequest("<h2>Bad Request</h2>")
+
+
+def m403(request):
+    return HttpResponseForbidden("<h2>Forbidden</h2>")
+
+
+def m404(request):
+    return HttpResponseNotFound("<h2>Not Found</h2>")
+
+
+def m405(request):
+    return HttpResponseNotAllowed("<h2>Method is not allowed</h2>")
+
+
+def m410(request):
+    return HttpResponseGone("<h2>Content is no longer here</h2>")
+
+
+def m500(request):
+    return HttpResponseServerError("<h2>Something is wrong</h2>")
